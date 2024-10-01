f3d18f70a74cc8559d66195d9a8b646a3d56c0c8
albe194e <albe194e@stud.kea.dk>
2024-10-01 15:20:13 +0200

    Test test

--- backend/src/main/java/com/example/testmandatory1/Person.java
+++ backend/src/main/java/com/example/testmandatory1/Person.java
@@ -2,4 +2,15 @@ package com.example.testmandatory1;
 
 
 public class Person {
+
+    private String cpr;
+    private String firstName;
+    private String lastName;
+    private char gender;
+
+    private String dob;
+
+    private Address address;
+
+    private String phoneNumber;
 }
