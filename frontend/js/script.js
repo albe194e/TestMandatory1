e7f8e4662cfd8555d8a0106f9bbbda88090f6689
DanielStarckLorenzen <99916244+DanielStarckLorenzen@users.noreply.github.com>
2024-10-16 14:32:54 +0200

    cache updates on save

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,3 +1,15 @@
-import { baseUrl } from "./info.js";
+const baseUrl = 'http://localhost/8080';
+
+document.getElementById('frmGenerate').addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  const isPartial = document.getElementById('chkPartialOptions').checked;
+  const numberOfPersons = document.getElementById('txtNumberOfPersons').value;
+  const partialOptions = document.getElementById('cmbPartialOptions').value;
+
+  let endpoint = '/';
+
+  alert('isPartial: ' + isPartial + ' numberOfPersons: ' + numberOfPersons + ' partialOptions: ' + partialOptions);
+});
 
 
